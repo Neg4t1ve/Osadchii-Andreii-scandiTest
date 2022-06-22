db255cd01c6f24e0ce7ddcc1bd80bc1638e46579
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-23 01:29:49 +0400

    basic markup and style of plp

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,7 +1,8 @@
+import Main from "components/Main/Main";
 import React, { Component } from "react";
 
 export default class ProductDetailPage extends Component {
   render() {
-    return <div>ProductDetailPage</div>;
+    return <Main>ProductDetailPage</Main>;
   }
 }
--- src/pages/ProductLandingPage.jsx
+++ src/pages/ProductLandingPage.jsx
@@ -1,7 +1,24 @@
+import Card from "components/Card/Card";
+import Main from "components/Main/Main";
 import React, { Component } from "react";
+import styles from "./styles/landing.module.scss";
 
 export default class ProductLandingPage extends Component {
   render() {
-    return <div>ProductLandingPage</div>;
+    return (
+      <Main>
+        <div className={styles.container}>
+          <h2 className={styles.title}>Category name</h2>
+          <div className={styles.grid}>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+        </div>
+      </Main>
+    );
   }
 }
