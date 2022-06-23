0b88ea377f12067f1708105e2cde589b4e4e797a
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-23 18:25:29 +0400

    basic product card markup

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
+import styles from "./card.module.scss";
 
 export default class Card extends Component {
   render() {
-    return <div>Card</div>;
+    return (
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <img src={this.props.img} alt="#" />
+        </div>
+        <div className={styles.textContainer}>
+          <h3 className={styles.title}>Apollo Running Short</h3>
+          <p className={styles.price}>$50.00</p>
+        </div>
+      </div>
+    );
   }
 }
