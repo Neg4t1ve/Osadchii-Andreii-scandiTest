9e424206e0af8a0b7f7456dda8694c97b82e8879
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-24 17:29:55 +0400

    make divider

---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import styles from "./divider.module.scss";
+
+export default class Divider extends Component {
+  render() {
+    return <div className={styles.divider}></div>;
+  }
+}
