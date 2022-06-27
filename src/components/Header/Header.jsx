e6446c94d9b8a2fbc21db084cb1ea82e2ed3acf3
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-27 19:10:31 +0400

    make comp for categories

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,15 @@ import React, { Component } from "react";
 import logo from "../../assets/img/logo.svg";
 import styles from "./header.module.scss";
 import MiniCart from "components/MiniCart/MiniCart";
-import CurrencySwitcher from "components/CurrencySwitcher/CurrencySwitcher";
+import CurrencySwitcher from "components/Header/CurrencySwitcher/CurrencySwitcher";
+import Categories from "./Categories/Categories";
 
 export default class Header extends Component {
   render() {
     return (
       <header>
         <div className={styles.container}>
-          <div className={styles.categories}>
-            <div className={styles.Category_primary}>WOMAN</div>
-            <div className={styles.Category}>MAN</div>
-            <div className={styles.Category}>KIDS</div>
-          </div>
-
+          <Categories />
           <img className={styles.logo} src={logo} alt="logo" />
 
           <div className={styles.actions}>
