1fdeeb1b987f57a2811061bf36639cec3e0991e4
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-28 21:05:34 +0400

    fetch data from be, pass it to card

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -2,26 +2,67 @@ import Card from "components/Card/Card";
 import Main from "components/Main/Main";
 import React, { Component } from "react";
 import styles from "./styles/listing.module.scss";
-import test1 from "../assets/img/test.jpg";
-import test2 from "../assets/img/test2.jpg";
-import test3 from "../assets/img/test3.jpg";
+import { connect } from "react-redux";
+import { client } from "api/client";
+import { GET_PRODUCTS_BY_CATEGORY } from "api/queries/GET_PRODUCTS_BY_CATEGORY";
 
-export default class ProductListingPage extends Component {
+const mapStateToProps = (state) => ({
+  category: state.category.activeCategory,
+});
+
+class ProductListingPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+    };
+
+    this.fetchProducts = this.fetchProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchProducts();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.category !== this.props.category) {
+      this.fetchProducts();
+    }
+  }
+
+  async fetchProducts() {
+    const result = await client.query({
+      query: GET_PRODUCTS_BY_CATEGORY,
+      variables: {
+        title: this.props.category,
+      },
+    });
+    this.setState({ products: result.data.category.products });
+  }
+
   render() {
     return (
       <Main>
         <div className={styles.container}>
-          <h2 className={styles.title}>Category name</h2>
+          <h2 className={styles.title}>{this.props.category.toUpperCase()}</h2>
           <div className={styles.grid}>
-            <Card img={test1} />
-            <Card img={test2} />
-            <Card img={test3} />
-            <Card img={test1} />
-            <Card img={test3} />
-            <Card img={test2} />
+            {this.state.products.map((product) => (
+              <Card
+                id={product.id}
+                key={product.brand + product.name}
+                inStock={product.inStock}
+                name={product.name}
+                brand={product.brand}
+                img={product.gallery[0]}
+                price={product.prices[0].amount}
+              />
+            ))}
           </div>
         </div>
       </Main>
     );
   }
 }
+
+export default connect(mapStateToProps)(ProductListingPage);
