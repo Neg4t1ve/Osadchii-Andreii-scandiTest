313d672866e1aa3a726ffeb31d02b1d7d84da827
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-29 21:55:52 +0400

    query for attr

--- src/api/queries/GET_ATTRIBUTES.js
+++ src/api/queries/GET_ATTRIBUTES.js
@@ -1,7 +1,7 @@
 const { gql } = require("@apollo/client");
 
 export const GET_ATTRIBUTES = gql`
-  query GET_ATTRIBUTES($id: String! = "ps-5") {
+  query GET_ATTRIBUTES($id: String!) {
     product(id: $id) {
       attributes {
         id
