b48256f8be92d7f82ddeaf8a467f840e322a51f6
Andrei Osadchii <nc1xo099@gmail.com>
2022-07-13 23:51:47 +0400

    made minicart modal

--- src/components/MiniCart/MiniCart.jsx
+++ src/components/MiniCart/MiniCart.jsx
@@ -1,12 +1,82 @@
+import Product from "components/Cart/Product";
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import cart from "../../assets/img/EmptyCart.svg";
+import styles from "./minicart.module.scss";
 
-export default class MiniCart extends Component {
+const mapStateToProps = (state) => ({
+  cart: state.cart.products,
+  currency: state.cart.activeCurrency,
+});
+
+class MiniCart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibility: false,
+    };
+    this.toggleCartVisibility = this.toggleCartVisibility.bind(this);
+  }
+
+  toggleCartVisibility() {
+    this.setState((state) => ({
+      visibility: !state.visibility,
+    }));
+  }
+
   render() {
     return (
-      <div>
-        <img src={cart} alt="cart" />
+      <div className={styles.miniCartContainer}>
+        <button
+          className={styles.miniCartSwitcher}
+          onClick={this.toggleCartVisibility}
+        >
+          <img src={cart} alt="minicart" />
+        </button>
+
+        {this.state.visibility && (
+          <>
+            <div
+              className={styles.basketBackground}
+              onClick={this.toggleCartVisibility}
+            >
+              <div className={styles.basket}>
+                <div className={styles.basketContainer}>
+                  {this.props.cart &&
+                    this.props.cart.map((item) => {
+                      return (
+                        <Product
+                          productId={item.productId}
+                          key={item.productId + item.count}
+                          activeAttr={item.activeAttr}
+                          count={item.count}
+                          isFull={true}
+                          productName={item.productName}
+                          brand={item.brand}
+                          gallery={item.gallery}
+                          prices={item.prices}
+                          attributes={item.attributes}
+                        />
+                      );
+                    })}
+                  <Link onClick={this.toggleCartVisibility} to="/cart">
+                    CART
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     );
   }
 }
+
+// {this.props.cart &&
+//   this.props.cart.map((item) => {
+//     return <Product productId={item} key={item} isFull={false} />;
+//   })}
+
+export default connect(mapStateToProps)(MiniCart);
