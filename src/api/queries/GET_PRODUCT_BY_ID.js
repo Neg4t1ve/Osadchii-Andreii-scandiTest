e958c855e629de7986af092f2deb7ff408ffd987
Andrei Osadchii <nc1xo099@gmail.com>
2022-07-15 03:14:42 +0400

    get id

--- src/api/queries/GET_PRODUCT_BY_ID.js
+++ src/api/queries/GET_PRODUCT_BY_ID.js
@@ -3,6 +3,7 @@ const { gql } = require("@apollo/client");
 export const GET_PRODUCT_BY_ID = gql`
   query GET_PRODUCT_BY_ID($id: String!) {
     product(id: $id) {
+      id
       name
       gallery
       brand
