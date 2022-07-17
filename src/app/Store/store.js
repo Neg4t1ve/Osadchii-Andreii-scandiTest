e3ba57a93d4aa2c54b85bce76b212d8113dac9aa
Andrei Osadchii <nc1xo099@gmail.com>
2022-07-18 02:12:51 +0400

    add redux-presist

---
+++ src/app/Store/store.js
@@ -0,0 +1,40 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import categoriesReducer from "..//Slices/categoriesSlice";
+import cartReducer from "../Slices/cartSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  category: categoriesReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import store, { persistor } from "./app/Store/store";
 import App from "./App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "api/client";
+import { PersistGate } from "redux-persist/integration/react";
+import Loader from "components/Loader/Loader";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -14,11 +16,13 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApolloProvider client={client}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ApolloProvider>
+      <PersistGate persistor={persistor} loading={<Loader />}>
+        <ApolloProvider client={client}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ApolloProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
