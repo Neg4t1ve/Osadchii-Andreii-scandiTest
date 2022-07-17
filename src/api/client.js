689a70766a685f82a715115b373f2a02e2b3fb99
Andrei Osadchii <nc1xo099@gmail.com>
2022-07-18 01:04:51 +0400

    no-cache option for apolloclient

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,18 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
 export const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
+  defaultOptions,
 });
