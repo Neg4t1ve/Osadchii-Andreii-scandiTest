e7a531b6895a429731076487986aa654a60ec36b
Andrei Osadchii <nc1xo099@gmail.com>
2022-07-18 01:00:41 +0400

    add version for minicart

--- src/components/Details/Attributes/Attributes.jsx
+++ src/components/Details/Attributes/Attributes.jsx
@@ -9,18 +9,65 @@ export default class Attributes extends Component {
   }
 
   swatch(HEX) {
-    return { backgroundColor: `${HEX}`, height: "2rem", width: "2rem" };
+    return this.props.isFull
+      ? { backgroundColor: `${HEX}`, height: "2rem", width: "2rem" }
+      : { backgroundColor: `${HEX}`, height: "1rem", width: "1rem" };
   }
 
   render() {
+    if (this.props.isFull) {
+      return (
+        <ul className={styles.attributesContainer}>
+          {this.props.attributes &&
+            this.props.attributes.map((attribute) => {
+              return (
+                <div className={styles.attribute} key={attribute.id}>
+                  <h5 className={styles.attributeName}>{attribute.name}:</h5>
+                  <div className={styles.btnContainer}>
+                    {attribute.items &&
+                      attribute.items.map((item) => {
+                        const swatch = attribute.type === "swatch";
+                        return (
+                          <button
+                            className={
+                              swatch
+                                ? this.props.activeAttr?.[attribute.name] ===
+                                  item.value
+                                  ? styles.swatchActive
+                                  : styles.swatch
+                                : this.props.activeAttr?.[attribute.name] ===
+                                  item.value
+                                ? styles.buttonActive
+                                : styles.button
+                            }
+                            key={item.id}
+                            id={attribute.name}
+                            value={item.value}
+                            onClick={this.props.pickAttribute}
+                          >
+                            {swatch ? (
+                              <div style={this.swatch(item.value)}></div>
+                            ) : (
+                              item.value
+                            )}
+                          </button>
+                        );
+                      })}
+                  </div>
+                </div>
+              );
+            })}
+        </ul>
+      );
+    }
     return (
-      <div className={styles.attributesContainer}>
+      <ul className={styles.miniAttributesContainer}>
         {this.props.attributes &&
           this.props.attributes.map((attribute) => {
             return (
-              <div className={styles.attribute} key={attribute.id}>
-                <h5 className={styles.attributeName}>{attribute.name}:</h5>
-                <div className={styles.btnContainer}>
+              <div className={styles.miniAttribute} key={attribute.id}>
+                <h5 className={styles.miniAttributeName}>{attribute.name}:</h5>
+                <div className={styles.miniBtnContainer}>
                   {attribute.items &&
                     attribute.items.map((item) => {
                       const swatch = attribute.type === "swatch";
@@ -30,20 +77,23 @@ export default class Attributes extends Component {
                             swatch
                               ? this.props.activeAttr?.[attribute.name] ===
                                 item.value
-                                ? styles.swatchActive
-                                : styles.swatch
+                                ? styles.miniSwatchActive
+                                : styles.miniSwatch
                               : this.props.activeAttr?.[attribute.name] ===
                                 item.value
-                              ? styles.buttonActive
-                              : styles.button
+                              ? styles.miniButtonActive
+                              : styles.miniButton
                           }
                           key={item.id}
                           id={attribute.name}
                           value={item.value}
                           onClick={this.props.pickAttribute}
-                          style={swatch ? this.swatch(item.value) : null}
                         >
-                          {swatch ? null : item.value}
+                          {swatch ? (
+                            <div style={this.swatch(item.value)}></div>
+                          ) : (
+                            item.value
+                          )}
                         </button>
                       );
                     })}
@@ -51,7 +101,7 @@ export default class Attributes extends Component {
               </div>
             );
           })}
-      </div>
+      </ul>
     );
   }
 }
